188b9bb5b73b54ac153115707be8a43327849138
LautaroConejero <161512966+LautaroConejero@users.noreply.github.com>
2025-06-03 16:34:49 -0300

    a

---
+++ Ej_2/headers/Drones.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <memory>
+#include <vector>
+#include <utility>
+#include <unordered_map>
+#include <thread>
+#include <mutex>
+#include <chrono>
+using namespace std;
+
+class Simulacion{};
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 veri
 ej1 ponerle la justificacion
 
+pregutna en el 1 como funciona la pokedex, si hay que comparar tmb con experiencia y por nivel para verificar si tengo ese pokemon
