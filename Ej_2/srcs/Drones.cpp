8dd555f8c55d43c268c72eaf82ab1a136e900698
LautaroConejero <161512966+LautaroConejero@users.noreply.github.com>
2025-06-06 10:53:44 -0300

    gonza rompe pelotas

--- Ej_2/srcs/Drones.cpp
+++ Ej_2/srcs/Drones.cpp
@@ -8,16 +8,9 @@ void SimDespegue::Despegue(int dron) {
     cout << "\nDron " << dron << " esperando para despegar..." << endl;   
     z_turb[0].unlock();
 
-    bool locked1 = z_turb[dron].try_lock();
-    bool locked2 = z_turb[(dron % 5) + 1].try_lock();
+    z_turb[dron].lock();
+    z_turb[(dron % 5) + 1].lock();
 
-    while (!locked1 || !locked2) {
-        if (locked1) z_turb[dron].unlock();
-        if (locked2) z_turb[(dron % 5) + 1].unlock();
-        
-        locked1 = z_turb[dron].try_lock();
-        locked2 = z_turb[(dron % 5) + 1].try_lock();
-    }
     z_turb[0].lock();
     cout << "\nDron " << dron << " despegando..." << endl;
     z_turb[0].unlock();
@@ -26,7 +19,7 @@ void SimDespegue::Despegue(int dron) {
     z_turb[0].lock();
     cout << "\nDron " << dron << " alcanzo la altura de 10m" << endl;
     z_turb[0].unlock();
-    
+
     z_turb[dron].unlock();
     z_turb[(dron % 5) + 1].unlock();
     
