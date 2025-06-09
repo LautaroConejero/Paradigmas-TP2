4b30a6df15935f08492d968944c69d778b3d1d70
LautaroConejero <161512966+LautaroConejero@users.noreply.github.com>
2025-06-09 10:48:04 -0300

    mini correccion, evitamos deadlock

--- Ej_2/srcs/Drones.cpp
+++ Ej_2/srcs/Drones.cpp
@@ -8,8 +8,10 @@ void SimDespegue::Despegue(int dron) {
     cout << "\nDron " << dron << " esperando para despegar..." << endl;   
     z_turb[0].unlock();
 
-    lock_guard<mutex> turb_izq(z_turb[dron]);
-    lock_guard<mutex> turb_der(z_turb[(dron % 5) + 1]);
+
+    lock(z_turb[dron], z_turb[(dron % 5) + 1]);
+    std::lock_guard<mutex> turb_izq(z_turb[dron], adopt_lock);
+    std::lock_guard<mutex> turb_der(z_turb[(dron % 5) + 1], std::adopt_lock);
 
     z_turb[0].lock();
     cout << "\nDron " << dron << " despegando..." << endl;
