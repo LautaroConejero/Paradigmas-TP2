a63bf74133264d59ce878b20ad33aa61484fc432
simon-howlin <showlin@udesa.edu.ar>
2025-06-11 18:26:31 -0300

    ej 3

--- Ej_3/main.cpp
+++ Ej_3/main.cpp
@@ -12,45 +12,64 @@
 #include <queue>
 using namespace std;
 
+#define sensoresCantidad 3
+#define robotsCantidad 3
+
+
 mutex gen_tarea;
 mutex proc_tarea;
+
+// conteiners 
 queue<Tarea> tareas;
 vector<jthread> sensores;
 vector<Jthread> robots;
-bool notificado = false;
-condition_variable tarea_cond;
+
+// lo del condition
+int sensorCompleto = 0;
+condition_variable cv;
 
 struct Tarea {
     int IdSensor;
     int IdTarea;
     string Descripcion; 
-    Tarea(int idSensor, int idTarea)
-        : IdSensor(idSensor), IdTarea(idTarea), Descripcion("Tarea " + to_string(idTarea)) {}
+    Tarea(int idSensor, int idTarea): IdSensor(idSensor), IdTarea(idTarea), Descripcion("Tarea " + to_string(idTarea)) {}
 };
 
-Tarea generar_tarea(int idSensor) {
+void generar_tarea(int idSensor) {
     unique_lock<mutex> lock(gen_tarea);
-    int idTarea = rand() % 100; 
-    Tarea nuevaTarea(idSensor, idTarea);
-    this_thread::sleep_for(chrono::milliseconds(175)); 
-    cout << "Sensor " << idSensor << " ha generado la tarea " << idTarea << endl;
-    tareas.push(nuevaTarea);
+
+    int cantTareas = rand() % 6 + 1;
+    for (int i = 0; i < cantTareas; i++) {
+        this_thread::sleep_for(chrono::milliseconds(175)); // sensores duermiendo
+
+        int idTarea = rand() % 100; 
+        Tarea nuevaTarea(idSensor, idTarea);
+        tareas.push(nuevaTarea);
+        cout << "Sensor " << idSensor << " ha generado la tarea " << idTarea << endl;
+
+        cv.notify_one(); // se avisa de una tarea disponible a un thread (robot)
+    }
+
+    sensorCompleto++; // el sensor i-esimmo ya temrino de crear las n tareas (suma 1 a la cuenta para tener registro de cuantos sensores terminaron)
+    cv.notify_all(); // al terminar avisa a todos los threads (robots) que las tareas ya etsan disponibles para hacerlas
 }
 
-void procesar_tarea(int idRobot){
-    unique_lock<mutex> lock(proc_tarea);
-    while(!tareas.empty()){
-        lock.lock();
+void procesar_tarea(int idRobot) {
+    while(!tareas.empty() && !(sensorCompleto == sensoresCantidad)) {
+        unique_lock<mutex> lock(proc_tarea);
+        this_thread::sleep_for(chrono::milliseconds(250)); // robots durmiendo
+
+        cv.wait(lock, []{return !tareas.empty() || sensorCompleto == sensoresCantidad;}); // Espera hasta que haya tareas o todos los sensores hayan terminado
+
         Tarea tarea = tareas.front();
         tareas.pop();
-        this_thread::sleep_for(chrono::milliseconds(250));
         cout << "Robot " << idRobot << " Termino de procesar la tarea " << tarea.Descripcion << " del sensor " << tarea.IdSensor << endl;
-        lock.unlock();
-\    }
 
+        lock.unlock();
+    }
 }
 
-void Monitoreo(){
+void Monitoreo() {
     srand(time(0)); 
     for (int i = 0; i < 3; ++i) {
         sensores.push_back(jthread(&generar_tarea, i));
@@ -59,4 +78,9 @@ void Monitoreo(){
         robots.push_back(jthread(&procesar_tarea, i));
     }
 
+}
+
+int main() {
+    Monitoreo();
+    return 0;
 }
